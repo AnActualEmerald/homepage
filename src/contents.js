bd81ca95c909902ff559382a2088a01155fca4c1
AnActualEmerald <kevinliger13@gmail.com>
2021-06-15 02:53:51 -0400

    Made the picture scroll thing in the middle

---
+++ src/contents.js
@@ -0,0 +1,6 @@
+import shesNeat from '/src/res/content/thinkShesNeat.jpg';
+import missingGem from '/src/res/content/missingGem.png';
+
+export function getImages(){
+    return [shesNeat, missingGem];
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("ready");
+import {getImages} from './contents.js'
+
+let current = 0;
+let content = getImages();
+
+
+function setContent(index) {
+    let placeholder = document.querySelector("#content-placeholder");
+    placeholder.setAttribute("src", content[index]);
+}
+
+
+function moveContentLeft() {
+    current--;
+    if(current < 0) current = content.length - 1;
+    setContent(current);
+}
+
+function moveContentRight() {
+    current++;
+    if(current >= content.length) current = 0;
+
+    setContent(current);
+}
+
+window.onload = () => {
+    setContent(0);
+    document.getElementById("left").onclick = moveContentLeft;
+    document.getElementById("right").onclick = moveContentRight;
+}
