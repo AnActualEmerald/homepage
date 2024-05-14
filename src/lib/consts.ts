75b1b8c489de4e0ffd975434bbf0d89bafb07e49
AnActualEmerald <emerald@mecha.garden>
2024-05-14 12:40:34 -0400

    purge the javascript

--- src/lib/consts.ts
+++
@@ -1,6 +0,0 @@
-import { dev } from "$app/environment";
-
-export const TITLE = "Verdant Homeland";
-export const DESCRIPTION = "AAAAAAAAAAAAAAAAAAAAAAAAAA";
-export const URL = dev ? "https://5173.code.greenboi.me" : "https://greenboi.me";
-
--- src/routes/posts/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { Post } from '$lib/types';
-import type { PageServerLoad } from './$types';
-
-export const load = (async ({ fetch }) => {
-	const posts : Post[] = await (await fetch('/api/posts')).json();
-	return {
-		posts
-	};
-}) satisfies PageServerLoad;
