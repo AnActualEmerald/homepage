5e8b7a113f3f03e8c6efa558612c86fca15ad4af
emerald <emerald_actual@proton.me>
2025-04-09 21:58:59 -0400

    tweak open-props hosting, robots.txt

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 import autoprefixer from 'autoprefixer';
 import customMedia from 'postcss-custom-media';
 import postcssImport from 'postcss-import';
+import postcssJitProps from 'postcss-jit-props';
+import OpenProps from 'open-props';
 import { cwd } from 'node:process';
 
 export default {
@@ -9,6 +11,7 @@ export default {
       from: cwd() + "/assets/css/main.css",
       path: [cwd() + "/assets/css", cwd() + "/node_modules"]
     }),
+    postcssJitProps(OpenProps),
     autoprefixer,
     customMedia,
   ]
